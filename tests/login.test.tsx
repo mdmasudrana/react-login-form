7ae29c293f3bd153ba32c9fce4ffb3113a95651f
mdmasudrana <kazimasud6050@gmail.com>
2023-08-20 13:53:33 +0600

    commit 2

--- tests/login.test.tsx
+++ tests/login.test.tsx
@@ -6,9 +6,7 @@ import LoginComponent from '../src/Component/Login';
 
 describe('LoginComponent', () => {
   it('should render login inputs', () => {
-    // Declare the wrapper variable here
     const wrapper = mount(<LoginComponent />);
-    // Perform assertions using chai's expect
     expect(wrapper.find('input[type="text"]').length).to.equal(1);
     expect(wrapper.find('input[type="password"]').length).to.equal(1);
   });
